c9d883d1f7ffeb5550d298357ab914ecdae3d1f6
BONTHALASOMESHKUMAR <someshkumarbonthala@gmial.com>
2026-03-12 09:49:38 +0530

    UC5:Booking Request Queue

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,5 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class BookMyStayApp {
 
@@ -43,7 +42,7 @@ public class BookMyStayApp {
         }
     }
 
-    // RoomInventory class
+    // Room Inventory (Use Case 3)
     static class RoomInventory {
 
         private Map<String, Integer> roomAvailability;
@@ -100,15 +99,57 @@ public class BookMyStayApp {
         }
     }
 
+    // Reservation class (Use Case 5)
+    static class Reservation {
+
+        private String guestName;
+        private String roomType;
+
+        public Reservation(String guestName, String roomType) {
+            this.guestName = guestName;
+            this.roomType = roomType;
+        }
+
+        public String getGuestName() {
+            return guestName;
+        }
+
+        public String getRoomType() {
+            return roomType;
+        }
+    }
+
+    // Booking Request Queue (FIFO)
+    static class BookingRequestQueue {
+
+        private Queue<Reservation> requestQueue;
+
+        public BookingRequestQueue() {
+            requestQueue = new LinkedList<>();
+        }
+
+        public void addRequest(Reservation reservation) {
+            requestQueue.offer(reservation);
+        }
+
+        public Reservation getNextRequest() {
+            return requestQueue.poll();
+        }
+
+        public boolean hasPendingRequests() {
+            return !requestQueue.isEmpty();
+        }
+    }
+
     // Main Method
     public static void main(String[] args) {
 
+        // Use Case 4: Room Search
         Room singleRoom = new SingleRoom();
         Room doubleRoom = new DoubleRoom();
         Room suiteRoom = new SuiteRoom();
 
         RoomInventory inventory = new RoomInventory();
-
         RoomSearchService searchService = new RoomSearchService();
 
         searchService.searchAvailableRooms(
@@ -116,5 +157,24 @@ public class BookMyStayApp {
                 singleRoom,
                 doubleRoom,
                 suiteRoom);
+
+        // Use Case 5: Booking Request Queue
+        System.out.println("Booking Request Queue\n");
+
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
+
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        while (bookingQueue.hasPendingRequests()) {
+            Reservation r = bookingQueue.getNextRequest();
+            System.out.println("Processing booking for Guest: "
+                    + r.getGuestName() + ", Room Type: " + r.getRoomType());
+        }
     }
 }
